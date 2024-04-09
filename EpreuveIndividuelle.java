a4547ba688e7e5f374fb8c8369d8124067a169fc
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2024-04-09 15:02:09 +0200

    Implémentation du squelette de la classe EpreuveIndividuelle

--- EpreuveIndividuelle.java
+++ EpreuveIndividuelle.java
@@ -1,3 +1,8 @@
-public class EpreuveIndividuelle {
+public class EpreuveIndividuelle extends Epreuves {
+    
+    /** Constructeur qui met à jour les attributs de la classe EpreuveIndividuelle */
+    public EpreuveIndividuelle(String nom, int scoreMax, String sexe) {
+        super(nom, scoreMax, sexe);
+    }
     
 }
