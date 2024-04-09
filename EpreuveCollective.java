8100666779e2b31768d950f2db372e1e1db1fb88
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2024-04-09 15:02:50 +0200

    Implémentation du squelette de la classe EpreuveCollective

--- EpreuveCollective.java
+++ EpreuveCollective.java
@@ -1,3 +1,7 @@
-public class EpreuveCollective {
+public class EpreuveCollective extends Epreuves {
     
+    /** Constructeur qui met à jour les attributs de la classe EpreuveIndividuelle */
+    public EpreuveCollective(String nom, int scoreMax, String sexe) {
+        super(nom, scoreMax, sexe);
+    }
 }
