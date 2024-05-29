5427d919f3f18b1c275af24fd97e1226a90743fc
Khadjou Youssef <o22301854@info22-03.iut45.univ-orleans.fr>
2024-05-29 11:41:37 +0200

    commit pour les sport

--- src/Sport.java
+++ src/Sport.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public abstract class Sport {
     private List<Epreuve> lesEpreuves;
     private String nom;
+    private Map<Caracteristique , Double> LesCoefficients;
+    private String unite;
 
 
-    public Sport(String nom) {
+
+    public Sport(String nom , double force , double agilite , double endurance , String unite) {
         this.nom = nom;
         this.lesEpreuves = new ArrayList<>();
+
+        this.LesCoefficients = new HashMap<>();
+
+        this.LesCoefficients.put(Caracteristique.FORCE , force );
+        this.LesCoefficients.put(Caracteristique.AGILITE , agilite);
+        this.LesCoefficients.put(Caracteristique.ENDURANCE, endurance);
+
+        this.unite = unite;
     }
 
     public Sport(String nom, List<Epreuve> lesEpreuves) {
@@ -37,8 +49,8 @@ public abstract class Sport {
         this.nom = nom;
     }
 
-    public abstract Map<Caracteristique , Double> coeffcient();
-
-    
+    public Map<Caracteristique,Double> coeffcient(){     
+        return this.LesCoefficients;
+    }
 
 }
